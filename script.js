cbeb5b68515650ef6960bf26869e0ea8d9179a16
Likhita-Narra <narra.likhita2020@vitstudent.ac.in>
2021-08-26 17:39:38 +0530

    Added hide and reveal buttons and JS function

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ document.getElementById("sample").style.fontSize = "50px";
 function hideText() {
     document.getElementById("hide").style.display = "none";
 }
+
+function revealText() {
+    document.getElementById("reveal").style.display = "block";
+}
+

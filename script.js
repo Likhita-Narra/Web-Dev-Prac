75b67c9b613e4e8cf81322be56bfbf16f4c05bac
Likhita-Narra <narra.likhita2020@vitstudent.ac.in>
2021-08-27 09:20:07 +0530

    Practiced using Google APIs

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function revealText() {
     document.getElementById("reveal").style.display = "block";
 }
 
+function createMap() {
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {
+        centre = new google.maps.LatLng(51.5, -0.2),
+        zoom = 10
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+

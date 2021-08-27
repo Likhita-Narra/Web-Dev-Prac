69b9f5561ce31cd29b466d6908f508e50fddddd0
Likhita-Narra <narra.likhita2020@vitstudent.ac.in>
2021-08-27 18:46:34 +0530

    Added a simple Javascript functionality

---
+++ images/Javascript.js
@@ -0,0 +1,9 @@
+function displayDateTime() {
+    document.getElementById("Activator").innerHTML = Date();
+}
+
+function changeStyle() {
+    document.getElementById("name").style.fontSize = "30px";
+    document.getElementById("name").style.color = "yellow";
+    document.getElementById("name").style.backgroundColor = "pink";
+}

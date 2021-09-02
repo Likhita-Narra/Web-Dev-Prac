400d49512fd251757a6f44b0175ded675ac8ca22
Likhita-Narra <narra.likhita2020@vitstudent.ac.in>
2021-09-02 14:09:50 +0530

    Added a canvas

---
+++ canvas.js
@@ -0,0 +1,9 @@
+var c = document.getElementById("sample1");
+var ctx = c.getContext("2d");
+ctx.moveTo(0,0)
+ctx.lineTo(200, 100);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.stroke();

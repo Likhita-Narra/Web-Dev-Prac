2ef213fca4b8101a1928a0a9b39b3e8827b0d1e8
Likhita-Narra <narra.likhita2020@vitstudent.ac.in>
2021-09-02 14:27:22 +0530

    Added additional features to canvas

--- canvas.js
+++ canvas.js
@@ -7,3 +7,21 @@ ctx.stroke();
 ctx.beginPath();
 ctx.arc(95, 50, 40, 0, 2 * Math.PI);
 ctx.stroke();
+
+var c = document.getElementById("sample2");
+var ctx = c.getContext("2d");
+ctx.font = "30px Open Sans"
+ctx.fillText("Hello Likhita", 13, 60)
+
+var c = document.getElementById("sample3");
+var ctx = c.getContext("2d");
+ctx.font = "30px Open Sans"
+ctx.strokeText("Hello Likhita", 13, 60)
+
+var c = document.getElementById("sample4");
+var ctx = c.getContext("2d");
+var grd = ctx.createLinearGradient(0, 0, 200, 0);
+grd.addColorStop(0, "blue")
+grd.addColorStop(1, "violet")
+ctx.fillStyle = grd;
+ctx.fillRect(10, 10, 150, 80);
